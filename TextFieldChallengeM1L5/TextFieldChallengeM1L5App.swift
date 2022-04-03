3c593f37fbfe4ba69c43ff637d5904712509f4d8
agholson <87455899+agholson@users.noreply.github.com>
2022-04-03 10:07:40 -0400

    Initial Commit

---
+++ TextFieldChallengeM1L5/TextFieldChallengeM1L5App.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldChallengeM1L5App.swift
+//  TextFieldChallengeM1L5
+//
+//  Created by Leone on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldChallengeM1L5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
